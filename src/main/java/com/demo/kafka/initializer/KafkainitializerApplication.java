f7b96d9cf9f5f8f287570e4b75055dcaf18a0516
Vijay Satyanarayana <vijay.satyanarayana@Vijays-MacBook-Pro.local>
2020-11-02 11:06:16 -0800

    Updated

---
+++ src/main/java/com/demo/kafka/initializer/KafkainitializerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.kafka.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkainitializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkainitializerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/kafka/initializer/KafkainitializerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.kafka.initializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkainitializerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
